e0783c2c3bfd2f8c10b43c3730de9de45b0854dc
soommmin <150005268+soommmin@users.noreply.github.com>
2023-11-14 21:49:10 +0900

    카카오톡 로그인, 구글맵, Firebase 설정 사용자의 위치정보 표시하여 리액션 보내기

---
+++ app/src/main/java/android/part2_chapter9/EmailLoginActivity.kt
@@ -0,0 +1,29 @@
+package android.part2_chapter9
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.part2_chapter9.databinding.ActivityEmailLoginBinding
+import android.widget.Toast
+
+class EmailLoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEmailLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEmailLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty()) {
+                val data = Intent().apply {
+                    putExtra("email", binding.emailEditText.text.toString())
+                }
+                setResult(RESULT_OK, data)
+                finish()
+            } else {
+                Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
